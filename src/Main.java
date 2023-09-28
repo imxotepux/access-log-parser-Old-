a84327d45310da355adb88207329435a07b82be6
Aleksey Tishevich <ATishevich_in@inno.tech>
2023-09-28 17:21:08 +0300

    Updated text message

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите число:");
+        System.out.println("Введите первое число:");
         int firstNumber = new Scanner(System.in).nextInt();
-        System.out.println("Введите число:");
+        System.out.println("Введите второе число:");
         int secondNumber = new Scanner(System.in).nextInt();
 
         int summa = firstNumber + secondNumber;
